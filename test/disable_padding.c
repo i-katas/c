358c2ad65ffe00bbb63bf5e66f0bbd2c18201bdc
i-katas <feexonj@gmail.com>
2020-04-27 23:02:05 +0800

    extract utility method

--- test/disable_padding.c
+++ test/disable_padding.c
@@ -1,12 +1,28 @@
 #include <assert.h>
 #include <stdalign.h>
+#include <string.h>
+#include <stdlib.h>
+
 #define DISABLE_PADDING __attribute__((packed))
+
 
 struct NoPadding {
     char c;
     int i;
     double d;
 } DISABLE_PADDING;
+
+
+int get_int(char *ptr) {
+    return *ptr | *(ptr + 1) << 8 | *(ptr + 2) << 16 | *(ptr + 3) << 24;
+}
+
+
+double get_double(char *ptr) {
+    double value = 0;
+    return *(double *)memcpy(&value, ptr, sizeof(typeof(value)));
+}
+
 
 int main(void) {
     struct NoPadding s = {'a', 0x12345678, 100.23};
@@ -16,6 +32,6 @@ int main(void) {
     char *ptr = (char*) &s;
 
     assert(*ptr == 'a');
-    int value = *(ptr + 1) | *(ptr + 2) << 8 | *(ptr + 3) << 16 | *(ptr + 4) << 24;
-    assert(value == 0x12345678);
+    assert(get_int(ptr + 1) == 0x12345678);
+    assert(get_double(ptr + 5) == 100.23);
 }
